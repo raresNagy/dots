ad9b62cfb7207038a77a562810c6b044efdefb3a
Rares Nagy <rares.nagy@gmail.com>
2026-03-26 09:41:25 +0100

    Changed a few nvim options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,8 +1,15 @@
 vim.opt.ignorecase = true
 vim.o.smartcase = true
 
+
+vim.opt.expandtab = true-- Convert tabs to spaces
 vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 vim.opt.number = true
+vim.opt.smarttab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
 
 vim.opt.relativenumber = true
 vim.opt.smartindent = true
@@ -24,6 +31,10 @@ vim.opt.cursorline = true
 
 vim.opt.list = true
 vim.opt.listchars:append("trail:·")
+
+vim.opt.mouse = "a"
+
+vim.opt.signcolumn = "yes"
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.o.scrolloff = 10
