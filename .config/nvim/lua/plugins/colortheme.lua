652b47d6ed98f515f4bc936becd79d5588b91ac7
Rares Nagy <rares.nagy@gmail.com>
2026-04-15 17:29:20 +0200

    in neovim too

--- .config/nvim/lua/plugins/colortheme.lua
+++ .config/nvim/lua/plugins/colortheme.lua
@@ -4,21 +4,17 @@ return {
 		name = "moonfly",
 		lazy = false,
 		priority = 1000,
-		config = function()
-			vim.cmd([[colorscheme moonfly]])
-		end,
+		-- config = function()
+		-- 	vim.cmd([[colorscheme moonfly]])
+		-- end,
 	},
-	{
-		"projekt0n/github-nvim-theme",
-		name = "github-theme",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			require("github-theme").setup({
-				-- ...
-			})
 
-			vim.cmd("colorscheme github_dark")
+	{
+		"savq/melange-nvim",
+		name = "melange",
+		config = function()
+			vim.cmd([[colorscheme melange]])
 		end,
-	},
+	}
+	
 }
